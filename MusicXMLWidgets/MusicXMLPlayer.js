686dc135176e4d2e428219b6c4969309d8e16595
Alexey Agapov <alexey.agapov@perfectart.com>
2024-11-17 18:05:31 +0100

    loadScript moved to utils.js

--- MusicXMLWidgets/MusicXMLPlayer.js
+++ MusicXMLWidgets/MusicXMLPlayer.js
@@ -1,17 +1,4 @@
-function loadScript(url) {
-    if( document.querySelector(`script[src="${url}"]`)) {
-        // already loaded and ready to go
-        return Promise.resolve();
-    }
-
-    return new Promise(function(resolve, reject) {
-        var script = document.createElement("script");
-        script.onload = resolve;
-        script.onerror = reject;
-        script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
-    });
-}
+import {loadScript} from './utils.js'
 
 await Promise.all([
     loadScript('https://surikov.github.io/webaudiofont/npm/dist/WebAudioFontPlayer.js')
--- MusicXMLWidgets/MusicXMLViewer.js
+++ MusicXMLWidgets/MusicXMLViewer.js
@@ -1,48 +1,27 @@
-function loadScript(url) {
-    if( document.querySelector(`script[src="${url}"]`)) {
-        // already loaded and ready to go
-        return Promise.resolve();
-    }
-
-    return new Promise(function(resolve, reject) {
-        var script = document.createElement("script");
-        script.onload = resolve;
-        script.onerror = reject;
-        script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
-    });
-}
-
+import {loadScript} from './utils.js'
 
 await Promise.all([
     loadScript('https://unpkg.com/opensheetmusicdisplay@1.8.0/build/opensheetmusicdisplay.min.js')
 ]);
 
 function render({ model, el }) {
-
-    let osmd = ""
-
-    function renderHelper() {
-    
-
-    
-        osmd.load( model.get( 'xml')).then(() => {
-            osmd.render();
-        }).catch((error) => {
-            console.error('Error rendering MusicXML:', error);
-        });
-    }
-    
     const container = document.createElement('div');
     el.appendChild(container);
 
-    osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(container, {
+    const osmd  = new opensheetmusicdisplay.OpenSheetMusicDisplay(container, {
         autoResize: true,
         backend: 'svg',
         drawTitle: false,
         drawingParameters: "compacttight"
     });
 
+    function renderHelper() {
+        osmd.load( model.get( 'xml')).then(() => {
+            osmd.render();
+        }).catch((error) => {
+            console.error('Error rendering MusicXML:', error);
+        });
+    }
     renderHelper();
     model.on( 'change:xml', renderHelper);
 }
