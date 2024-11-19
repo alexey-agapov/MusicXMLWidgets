c9ca2b0fa6305e25a75c936b5bcd12af2df48f8d
Alexey Agapov <alexey.agapov@perfectart.com>
2024-11-19 05:24:04 +0100

    Removed utils, need to be addressed later

--- MusicXMLWidgets/MusicXMLPlayer.js
+++ MusicXMLWidgets/MusicXMLPlayer.js
@@ -1,4 +1,17 @@
-import {loadScript} from './utils.js'
+function loadScript(url) {
+    if( document.querySelector(`script[src="${url}"]`)) {
+        // already loaded and ready to go
+        return Promise.resolve();
+    }
+
+    return new Promise(function(resolve, reject) {
+        var script = document.createElement("script");
+        script.onload = resolve;
+        script.onerror = reject;
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    });
+}
 
 await Promise.all([
     loadScript('https://surikov.github.io/webaudiofont/npm/dist/WebAudioFontPlayer.js')
--- MusicXMLWidgets/MusicXMLViewer.js
+++ MusicXMLWidgets/MusicXMLViewer.js
@@ -1,4 +1,17 @@
-import {loadScript} from './utils.js'
+function loadScript(url) {
+    if( document.querySelector(`script[src="${url}"]`)) {
+        // already loaded and ready to go
+        return Promise.resolve();
+    }
+
+    return new Promise(function(resolve, reject) {
+        var script = document.createElement("script");
+        script.onload = resolve;
+        script.onerror = reject;
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    });
+}
 
 await Promise.all([
     loadScript('https://unpkg.com/opensheetmusicdisplay@1.8.0/build/opensheetmusicdisplay.min.js')
@@ -18,6 +31,22 @@ function render({ model, el }) {
     function renderHelper() {
         osmd.load( model.get( 'xml')).then(() => {
             osmd.render();
+            osmd.cursor.reset()
+            const iterator = osmd.cursor.iterator;
+
+            let currentTimeStamp = iterator.currentTimeStamp;
+            let tempoInBPM = iterator.currentMeasure.tempoInBPM;
+
+            while (!iterator.EndReached) {
+                currentTimeStamp = iterator.currentTimeStamp;
+                tempoInBPM = iterator.currentMeasure.tempoInBPM;
+                console.log( currentTimeStamp);
+                console.log( tempoInBPM);
+                console.log( iterator.currentMeasureIndex);
+                osmd.cursor.next()
+            }
+
+
         }).catch((error) => {
             console.error('Error rendering MusicXML:', error);
         });
